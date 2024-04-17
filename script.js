bd51f51ad97ea7c9a470c657a4044a380d3ddbb3
Fabio Mariantoni <spreadoibaf@gmail.com>
2024-04-17 22:36:36 +0200

    add script.js

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
 const html = document.documentElement
 
-console.log(html)
+const body = html.children[1]
+
+const ul = body.children[0]
+
+const ulParent = ul.parentElement
+
+console.log(ulParent)
+
+const secondLi = ul.children[1]
+
+console.log(secondLi)
+
+const nextLiFromSecond = secondLi.nextElementSibling.innerText;
+
+console.log(nextLiFromSecond)
+
+const previousLiFromSecond = secondLi.previousElementSibling.innerText;
+
+console.log(previousLiFromSecond)
